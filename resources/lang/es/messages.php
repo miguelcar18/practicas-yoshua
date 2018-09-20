717abc81e57c6681dad42ba61bd645eb58bd2604
yoshua7seven <yoshua.torres.07@gmail.com>
2018-09-20 09:30:09 -0400

    Configuraciones de DB y ejecucion de migraciones

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -1,1 +1,10 @@
-<?php return array();
+<?php return array (
+  'activity' => 'Actividad',
+  'activity_added' => 'Agregado',
+  'activity_deleted' => 'Eliminado',
+  'activity_generated' => 'Generado',
+  'activity_restored' => 'Restaurado',
+  'activity_switched' => 'Cambiado',
+  'activity_trashed' => 'Destrozado',
+  'activity_updated' => 'Actualizado',
+);
